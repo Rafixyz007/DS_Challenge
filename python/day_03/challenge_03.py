3d080f38ab8fa1fab95abd9c6e16818370d6bf30
Rafixyz007 <122726297+Rafixyz007@users.noreply.github.com>
2025-12-19 10:05:43 +0600

    Delete python directory

--- python/day_03/challenge_03.py
+++
@@ -1,9 +0,0 @@
-## 3. Micro Challenge The efficient skipper
-
-## Goal: Loop through numbers 1 to 10. print them, but if the number is 5, skip it and do not print anything.
-## Constraint: Use the continue keyword.
-
-for i in range(1,11):
-    if i == 5:
-        continue
-    print(i)
