f09c651e3e1d0037838bdfb19153e4ce5b93efbd
Rafixyz007 <122726297+Rafixyz007@users.noreply.github.com>
2025-12-20 10:17:07 +0600

    Delete challenge_04.py

--- challenge_04.py
+++
@@ -1,9 +0,0 @@
-## 4. Micro Challenge: The Custom Signal
-
-## Goal: Ask the user for a number. if the number negative, manually trigger an error using raise ValueError("No Negatives")
-
-
-user = int(input("Enter a number: "))
-
-if user < 0:
-    raise ValueError("No Negatives")
